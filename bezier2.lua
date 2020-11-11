0cbcec320c105f19e9f0fa5e6a1d37d3c56d40fc
Steven Johnson <steve@xibalbastudios.com>
2020-11-11 01:04:31 -0600

    arc_length moved out into tektite_core with some changes
    bezier module split up into bezier2, bezier3, and utils
    
    cubic broken up into convert, curve, and polynomial modules
    
    Bezier modules now include PopulateArcLength() routines
    
    Some renaming

--- bezier2.lua
+++ bezier2.lua
@@ -32,15 +32,19 @@
 
 -- Standard library imports --
 local abs = math.abs
-local ipairs = ipairs
 local log = math.log
 local sqrt = math.sqrt
 
+-- Modules --
+local arc_length = require("tektite_core.number.arc_length")
+local utils = require("spline_ops.utils")
+
 -- Cached module references --
-local _Bezier2_
-local _Bezier2_PosTan_
-local _Bezier2_Tan_
-local _Length2_
+local _GetPosition_
+local _GetPositionAndTangent_
+local _GetTangent_
+local _Length_
+local _Length_Array_
 
 -- Exports --
 local M = {}
@@ -56,7 +60,7 @@ local M = {}
 -- @number t Interpolation time, &isin; [0, 1].
 -- @treturn number Position x-coordinate...
 -- @treturn number ...and y-coordinate.
-function M.Bezier2 (p1, q, p2, t)
+function M.GetPosition (p1, q, p2, t)
 	local s = 1 - t
 	local a, b, c = s^2, 2 * s * t, t^2
 
@@ -67,20 +71,20 @@ end
 --
 --
 
---- Array variant of @{Bezier2}.
--- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{Bezier2}.
+--- Array variant of @{GetPosition}.
+-- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{GetPosition}.
 -- @number t Interpolation time, &isin; [0, 1].
 -- @treturn number Position x-coordinate...
 -- @treturn number ...and y-coordinate.
-function M.Bezier2_Array (bezier, t)
-	return _Bezier2_(bezier[1], bezier[2], bezier[3], t)
+function M.GetPosition_Array (bezier, t)
+	return _GetPosition_(bezier[1], bezier[2], bezier[3], t)
 end
 
 --
 --
 --
 
---- Variant of @{Bezier2} that also returns the tangent.
+--- Variant of @{GetPosition} that also returns the tangent.
 -- @tparam Vector p1 Endpoint #1...
 -- @tparam Vector q ...control point...
 -- @tparam Vector p2 ...and endpoint #2.
@@ -89,7 +93,7 @@ end
 -- @treturn number ...and y-coordinate.
 -- @treturn number Tangent x-component...
 -- @treturn number ...and y-component.
-function M.Bezier2_PosTan (p1, q, p2, t)
+function M.GetPositionAndTangent (p1, q, p2, t)
 	local s, t2 = 1 - t, 2 * t
 	local a, b, c = s^2, 2 * s * t, t^2
 	local px, py = a * p1.x + b * q.x + c * p2.x, a * p1.y + b * q.y + c * p2.y
@@ -103,42 +107,46 @@ end
 --
 --
 
---- Array variant of @{Bezier2_PosTan}.
--- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{Bezier2}.
+--- Array variant of @{GetPositionAndTangent}.
+-- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{GetPosition}.
 -- @number t Interpolation time, &isin; [0, 1].
 -- @treturn number Position x-coordinate...
 -- @treturn number ...and y-coordinate.
 -- @treturn number Tangent x-component...
 -- @treturn number ...and y-component.
-function M.Bezier2_PosTan_Array (bezier, t)
-	return _Bezier2_PosTan_(bezier[1], bezier[2], bezier[3], t)
+function M.GetPositionAndTangent_Array (bezier, t)
+	return _GetPositionAndTangent_(bezier[1], bezier[2], bezier[3], t)
 end
 
 --
 --
 --
 
---- Variant of @{Bezier2} that returns the tangent.
+--- Variant of @{GetPosition} that returns the tangent.
 -- @tparam Vector p1 Endpoint #1...
 -- @tparam Vector q ...control point...
 -- @tparam Vector p2 ...and endpoint #2.
 -- @number t Interpolation time, &isin; [0, 1].
 -- @treturn number Tangent x-component...
 -- @treturn number ...and y-component.
-function M.Bezier2_Tan (p1, q, p2, t)
+function M.GetTangent (p1, q, p2, t)
 	local t2 = 2 * t
 	local a, b, c = t2 - 2, 2 * (1 - t2), t2
 
 	return a * p1.x + b * q.x + c * p2.x, a * p1.y + b * q.y + c * p2.y
 end
 
---- Array variant of @{Bezier2_Tan}.
--- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{Bezier2}.
+--
+--
+--
+
+--- Array variant of @{GetTangent}.
+-- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{GetPosition}.
 -- @number t Interpolation time, &isin; [0, 1].
 -- @treturn number Tangent x-component...
 -- @treturn number ...and y-component.
-function M.Bezier2_Tan_Array (bezier, t)
-	return _Bezier2_PosTan_(bezier[1], bezier[2], bezier[3], t)
+function M.GetTangent_Array (bezier, t)
+	return _GetTangent_(bezier[1], bezier[2], bezier[3], t)
 end
 
 --
@@ -164,7 +172,7 @@ end
 -- _p2_? (For this purpose, _p1_ is considered to be on the left, _p2_ on the right.)
 -- @treturn number Position x-coordinate...
 -- @treturn number ...and y-coordinate.
-function M.GetControlPoint2 (p1, p2, below)
+function M.GetControlPoint (p1, p2, below)
 	return AuxGetControlPoint(p1.x, p1.y, p2.x, p2.y, below)
 end
 
@@ -178,10 +186,10 @@ end
 -- as two opposite corners of a rectangle, and one of the unused corners is chosen as the
 -- control point.
 --
--- Otherwise, the algorithm can either fall back to the behavior of @{GetControlPoint2} or
+-- Otherwise, the algorithm can either fall back to the behavior of @{GetControlPoint} or
 -- choose the midpoint of _p1_ and _p2_ (i.e. the spline degenerates to a line).
 --
--- For purposes of above / below, cf. _below_ in @{GetControlPoint2}.
+-- For purposes of above / below, cf. _below_ in @{GetControlPoint}.
 -- @tparam Vector p1 Endpoint #1...
 -- @tparam Vector p2 ...and #2.
 -- @number too_close If the difference (rather, its absolute value) between the x- or
@@ -191,11 +199,11 @@ end
 -- if _how_ is **"below"** or **"below\_else\_middle"**, or the one "above" otherwise.
 --
 -- Failing that, the midpoint is chosen as a fallback if _how_ is **"above\_else\_middle"**
--- or **below\_else\_middle"**. Otherwise, @{GetControlPoint2} is the fallback, with _below_
+-- or **below\_else\_middle"**. Otherwise, @{GetControlPoint} is the fallback, with _below_
 -- true if _how_ is **"below"**.
 -- @treturn number Position x-coordinate...
 -- @treturn number ...and y-coordinate.
-function M.GetControlPoint2_TryCorner (p1, p2, too_close, how)
+function M.GetControlPoint_TryCorner (p1, p2, too_close, how)
 	local x1, y1, x2, y2 = p1.x, p1.y, p2.x, p2.y
 
 	if abs(x2 - x1) > too_close and abs(y2 - y1) > too_close then
@@ -235,7 +243,7 @@ end
 -- @tparam Vector q ...interior control point...
 -- @tparam Vector p2 ...and endpoint #2.
 -- @treturn number Approximate arc length.
-function M.Length2 (p1, q, p2)
+function M.Length (p1, q, p2)
 	local p1x, p1y = p1.x, p1.y
 	local qpx, qpy = 2 * q.x - p1x, 2 * q.y - p1y
 	local ax, bx = p2.x - qpx, qpx - p1x
@@ -262,215 +270,48 @@ end
 --
 --
 
---- Array variant of @{Length2}.
--- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{Length2}.
+--- Array variant of @{Length}.
+-- @array bezier Elements 1, 2, 3 are interpreted as arguments _p1_, _q_, _p2_ from @{Length}.
 -- @treturn number Approximate arc length.
-function M.Length2_Array (bezier)
-	return _Length2_(bezier[1], bezier[2], bezier[3])
+function M.Length_Array (bezier)
+	return _Length_(bezier[1], bezier[2], bezier[3])
 end
 
 --
 --
 --
 
---[[
-	Length via split method
-
-	Earl Boebert's commentary, in original:
-
-	The last suggestion by Gravesen is pretty nifty, and I think it's a candidate for the
-	next Graphics Gems. I hacked out the following quick implementation, using the .h and
-	libraries definitions from Graphics Gems I (If you haven't got that book then you have
-	no business mucking with with this stuff :-)) The function "bezsplit" is lifted
-	shamelessly from Schneider's Bezier curve-fitter.
-]]
-
--- Workspace matrix: a triangle of vectors is used; the rest is dummied out --
-local Temp = {}
-
-for i = 1, 4 do
-	Temp[i] = {}
-
-	for j = 1, 4 do
-		Temp[i][j] = j <= 5 - i and {}
-	end
+local function Subdivide (bezier, left, right, t)
+	return utils.Subdivide(bezier, left, right, t, 2)
 end
 
--- Push a point onto the stack
-local V, Top = {}
+local Bezier = {}
 
-local function AddPoint (point)
-	V[Top + 1], V[Top + 2], Top = point.x, point.y, Top + 2
-end
-
--- Split a cubic bezier in two
-local function BezSplit ()
-	-- Copy control points.
-	local base = Top + 1
-
-	for _, temp in ipairs(Temp[1]) do
-		temp.x, temp.y, base = V[base], V[base + 1], base + 2
-	end
-
-	-- Triangle computation.
-	local prev_row = Temp[1]
-
-	for i = 2, 4 do
-		local row = Temp[i]
-
-		for j = 1, 5 - i do
-			local r, pr1, pr2 = row[j], prev_row[j], prev_row[j + 1]
-
-			r.x, r.y = .5 * (pr1.x + pr2.x), .5 * (pr1.y + pr2.y)
-		end
-
-		prev_row = row
-	end
-
-	-- Left split.
-	for i = 1, 4 do
-		AddPoint(Temp[i][1])
-	end
-
-	-- Right split.
-	for i = 1, 4 do
-		AddPoint(Temp[5 - i][i])
-	end
-end
-
--- Add polyline length if close enough
-local function AddIfClose (length, err)
-	-- Pop four points off the stack. Compute the point-to-point and chord lengths.
-	Top = Top - 8
-
-	local base = Top + 1
-	local x, y = V[base], V[base + 1]
-	local dx, dy = V[base + 6] - x, V[base + 7] - y
-
-	local len, main_len = 0, sqrt(dx^2 + dy^2)
-
-	for _ = 1, 3 do
-		dx, dy = V[base + 2] - x, V[base + 3] - y
-		len = len + sqrt(dx^2 + dy^2)
-		base, x, y = base + 2, x + dx, y + dy
-	end
-
-	-- If the point-to-point lengths sum to much more than the chord length, split up
-	-- the curve and sum the lengths of the two parts.
-	if len - main_len > err then
-		BezSplit()
-
-		local ll = AddIfClose(length, err)
-		local lr = AddIfClose(length, err)
-
-		len = ll + lr
-	end
-
-	return len
-end
-
---- Compute a (degree 3) [B&eacute;zier spline's length](http://steve.hollasch.net/cgindex/curves/cbezarclen.html).
--- @tparam Vector p1 Endpoint #1 of control polygon...
--- @tparam Vector q1 ...interior control point #1...
--- @tparam Vector q2 ...interior control point #2...
+--- Populate an arc &rarr; parameter lookup table given a (degree 2) B&eacute;zier spline.
+-- @array lut Lookup table, cf. @{tektite_core.number.sampling.Lookup}.
+-- @tparam Vector p1 Endpoint #1...
+-- @tparam Vector q ...control point...
 -- @tparam Vector p2 ...and endpoint #2.
--- @number tolerance "Close enough" separation between arc estimates and chord lengths.
--- @treturn number Approximate arc length.
-function M.Length3 (p1, q1, q2, p2, tolerance)
-	Top = 0
+-- @int[opt] nsamples Number of samples to load into _lut_. If absent, a default is used.
+-- @treturn number Total arc length.
+function M.PopulateArcLengthLUT (lut, p1, q, p2, nsamples)
+	Bezier[1], Bezier[2], Bezier[3] = p1, q, p2
 
-	AddPoint(p1)
-	AddPoint(q1)
-	AddPoint(q2)
-	AddPoint(p2)
+	local s = arc_length.PopulateLUT_Subdivide(lut, Bezier, Subdivide, nsamples, _Length_Array_)
 
-	return AddIfClose(0, tolerance)
+	Bezier[1], Bezier[2], Bezier[3] = nil
+
+	return s
 end
 
 --
 --
 --
 
---- Array variant of @{Length3}.
--- @array bezier Elements 1, 2, 3, 4 are interpreted as arguments _p1_, _q1_, _q2_, _p2_
--- from @{Length3}.
--- @number tolerance "Close enough" separation between arc estimates and chord lengths.
--- @treturn number Approximate arc length.
-function M.Length3_Array (bezier, tolerance)
-	Top = 0
-
-	for i = 1, 4 do
-		AddPoint(bezier[i])
-	end
-
-	return AddIfClose(0, tolerance)
-end
-
---
---
---
-
-do
-	local Row = {}
-
-	--- Subdivide a B&eacute;zier spline into two new splines, using [De Casteljau's algorithm](http://en.wikipedia.org/wiki/De_Casteljau's_algorithm).
-	-- @array bezier **Vector** elements 1, 2, ..., _deg_ + 1 corresponding to the first
-	-- endpoint, first control point, ..., final endpoint of the B&eacute;zier spline to subdivide.
-	--
-	-- It is safe to reuse the value of _bezier_ as either _dst1_ or _dst2_.
-	-- @array dst1 Receives the "left" subdivision, i.e. the spline is evaluated from 0 to
-	-- _t_ and **Vector** elements 1, 2, ..., _deg_ are populated with the results (tables
-	-- being created if necessary).
-	-- @array dst2 As per _dst1_, the "right" subdivision, evaluated from _t_ to 1.
-	-- @number[opt=.5] t Parameter at which to split the spline.
-	-- @int[opt=#bezier - 1] deg Degree of the spline.
-	function M.Subdivide (bezier, dst1, dst2, t, deg)
-		t = t or .5
-
-		-- The base of the De Casteljau triangle is just the source spline.
-		local n, height = 0, deg and deg + 1 or #bezier
-
-		for i = 1, height do
-			Row[n + 1] = bezier[i].x
-			Row[n + 2] = bezier[i].y
-
-			n = n + 2
-		end
-
-		-- Iterate up the triangle. The left-hand side, top to bottom, supplies the left
-		-- subdivision; the right-hand side, bottom to top, supplies the right.
-		local s = 1 - t
-
-		for i = 1, height do
-			local j = height - i + 1
-			local vl = dst1[i] or {}
-			local vr = dst2[j] or {}
-
-			vl.x, vr.x = Row[1], Row[n - 1]
-			vl.y, vr.y = Row[2], Row[n - 0]
-
-			dst1[i], dst2[j] = vl, vr
-
-			-- Generate the next row up by interpolating the working row. This can be
-			-- performed in place, since the left-hand interpoland will not be needed
-			-- after the current step and thus can be overwritten.
-			for k = 1, n - 2, 2 do
-				Row[k + 0] = s * Row[k + 0] + t * Row[k + 2]
-				Row[k + 1] = s * Row[k + 1] + t * Row[k + 3]
-			end
-
-			n = n - 2
-		end
-	end
-end
-
---
---
---
-
-_Bezier2_ = M.Bezier2
-_Bezier2_PosTan_ = M.Bezier2_PosTan
-_Bezier2_Tan_ = M.Bezier2_Tan
-_Length2_ = M.Length2
+_GetPosition_ = M.GetPosition
+_GetPositionAndTangent_ = M.GetPositionAndTangent
+_GetTangent_ = M.GetTangent
+_Length_ = M.Length
+_Length_Array_ = M.Length_Array
 
 return M
